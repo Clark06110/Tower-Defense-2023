1d69e05aff626a1fc61745159a23f46fa55369fd
Clark06110 <martel.clement5@gmail.com>
2023-01-22 13:03:58 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,100 @@
+const canvas = document.querySelector('canvas')
+const canvasContext = canvas.getContext('2d')
+
+canvas.width = 480
+canvas.height = 320
+
+canvasContext.fillStyle = 'white'
+canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+
+const mapImage = new Image()
+mapImage.onload = () => {
+    animate()
+}
+mapImage.src = 'assets/maps/map1.png'
+
+class Enemy {
+
+    constructor({ position = { x: 0, y: 0 } }) {
+        this.position = position
+        this.width = 20
+        this.height = 20
+        this.waypointIndex = 0
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+        this.center = {
+            x: this.position.x + this.width / 2,
+            y: this.position.y + this.height / 2
+        }
+    }
+
+    draw() {
+        canvasContext.fillStyle = 'red'
+        canvasContext.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        this.draw()
+
+        var waypoint = waypointsMap1[this.waypointIndex]
+        var yDistance = waypoint.y - this.center.y
+        var xDistance = waypoint.x - this.center.x
+
+        if ( yDistance < 0 ) {
+            this.velocity.y = -1
+        }
+        if ( yDistance > 0 ) {
+            this.velocity.y = 1
+        }
+        if ( xDistance < 0 ) {
+            this.velocity.x = -1
+        }
+        if ( xDistance > 0 ) {
+            this.velocity.x = 1
+        }
+
+
+        // const speed = 1
+        // this.velocity.x = Math.cos(angle) * speed
+        // this.velocity.y = Math.sin(angle) * speed
+
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        // var a = Math.abs(this.center.x - waypoint.x)
+        // var b = Math.abs(this.center.y - waypoint.y)
+
+        console.log()
+        console.log("waypoint -> ", waypoint)
+        console.log("this.center -> ", this.center)
+        console.log("yDistance -> ", yDistance)
+        console.log("xDistance -> ", xDistance)
+        console.log("this.position -> ", this.position)
+        console.log("this.velocity -> ", this.velocity)
+        console.log()
+
+        if ( Math.abs(this.center.x - waypoint.x) < 5 && Math.abs(this.center.y - waypoint.y) < 5 ) {
+            this.waypointIndex++
+          }
+        
+        console.log("INDEX", this.waypointIndex) 
+
+    }
+}
+
+const enemy1 = new Enemy({ position: { x: 50, y: 46 }})
+const enemy2 = new Enemy({ position: { x: 0, y: 46 }})
+
+function animate() {
+    requestAnimationFrame(animate)
+
+    canvasContext.drawImage(mapImage, 0, 0)
+    enemy1.update()
+    enemy2.update()
+}
+
+
+
+console.log("canvas", canvasContext)
